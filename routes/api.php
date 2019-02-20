8f21df0b09b93634340c1313a3024c79e8515888
Thiago Maciel <thiagomrvieira@gmail.com>
2019-02-20 15:03:47 -0300

    Middleware CORS

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('v1/noticias/', 'controladorNoticias@noticias');
+Route::get('v1/noticias/', 'controladorNoticias@noticias')->middleware('cors');;
 Route::get('v1/noticias/capa', 'controladorNoticias@capa');
 Route::get('v1/noticias/{id}', 'controladorNoticias@noticia');
 
